2c920edb5985dc3c6a53fe9c2070276aff5e4c93
jyeo83 <36876535+jyeo83@users.noreply.github.com>
2020-10-08 16:57:01 -0700

    Initial
    Check in

---
+++ GitTest/Database1/dbo/Tables/BidItems.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[BidItems] (
+    [BidItemId]            INT             IDENTITY (1, 1) NOT NULL,
+    [ProjectId]            INT             NOT NULL,
+    [ItemNo]               INT             NOT NULL,
+    [Code]                 NVARCHAR (MAX)  NULL,
+    [Description]          NVARCHAR (300)  NULL,
+    [UnitOfMeasure]        NVARCHAR (6)    NULL,
+    [EEQuantity]           DECIMAL (12, 3) NOT NULL,
+    [BidUnitPrice]         DECIMAL (13, 2) NOT NULL,
+    [IsFinalPay]           BIT             NOT NULL,
+    [CcoQuantity]          DECIMAL (12, 3) DEFAULT ((0.0)) NOT NULL,
+    [IsStructuresItem]     BIT             DEFAULT ((0)) NOT NULL,
+    [IsAdministrativeItem] BIT             DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_BidItems] PRIMARY KEY CLUSTERED ([BidItemId] ASC),
+    CONSTRAINT [FK_BidItems_Projects_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Projects] ([ProjectId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_BidItems_ProjectId]
+    ON [dbo].[BidItems]([ProjectId] ASC);
+
